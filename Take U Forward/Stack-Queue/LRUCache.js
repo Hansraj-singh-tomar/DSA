b272a9a2be0ce7cf7e197fdc9f4822b0d1df08f6
Hansraj-singh-tomar <hansrajput033@gmail.com>
2024-09-09 19:13:48 +0530

    comments added in lru cache question

--- Take U Forward/Stack-Queue/LRUCache.js
+++ Take U Forward/Stack-Queue/LRUCache.js
@@ -1,49 +1,39 @@
-// LRU cache implementation using js Map data structure
-
-// Note :- In that map last value is most recently used and first value is least recently used
-
-// GET
-// In get we have to return value from map or -1 if not found
-// Make it most recently used, for that we will have to delete it from map and add it at last
-
-// In set we have to insert value
-// In set firstly we have to check that,
-// if it is already present than we have to delete it from map
-// if capacity is full than we have to delete last element from map and add new one and make it most recently used
-// then we have to add it at last
-
-// Time complexity of get and set is O(1)
-
-
 class LRUCache {
     constructor(capacity) {
-        this.capacity = capacity;
-        this.map = new Map();
+        this.capacity = capacity; // Max number of items the cache can hold
+        this.cache = new Map(); // Store the cache items
     }
 
     get(key) {
-        if (this.map.has(key)) {
-            let value = this.map.get(key);
-            this.map.delete(key);
-            this.map.set(key, value);
-            return value;
+        if (!this.cache.has(key)) {
+            return -1; // If the key is not in the cache, return -1
         }
-        return -1;
+
+        // Move the accessed item to the end (most recently used)
+        const value = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, value);
+
+        return value; // Return the value associated with the key
     }
 
     put(key, value) {
-        if (this.map.has(key)) {
-            this.map.delete(key);
-        }
-        if (this.map.size >= this.capacity) {
-            this.map.delete(this.map.keys().next().value); // this will return the first key and delete it from the map 
+        if (this.cache.has(key)) {
+            // If the key is already in the cache, delete it to update the order
+            this.cache.delete(key);
+        } else if (this.cache.size >= this.capacity) {
+            // If the cache is full, remove the least recently used item (the first item in the Map)
+            const firstKey = this.cache.keys().next().value;
+            this.cache.delete(firstKey);
         }
 
-        this.map.set(key, value);
+        // Insert the item as the most recently used (at the end)
+        this.cache.set(key, value);
     }
 }
 
-const lru = new LRUCache(3);
+// Example usage:
+const lru = new LRUCache(3); // Create a cache with a capacity of 3
 
 lru.put(1, 1); // Cache is {1=1}
 lru.put(2, 2); // Cache is {1=1, 2=2}
@@ -56,5 +46,3 @@ lru.put(5, 5); // Cache is {4=4, 3=3, 5=5} (removes key 1)
 console.log(lru.get(1)); // Returns -1 (not found)
 console.log(lru.get(4)); // Returns 4, Cache is {3=3, 5=5, 4=4}
 console.log(lru.get(5)); // Returns 5, Cache is {3=3, 4=4, 5=5}
-console.log(lru); // {capacity: 3, map: Map {3 => 3, 4 => 4, 5 => 5}}
-
--- practice.js
+++ practice.js
@@ -1,60 +1,10 @@
-// LRU cache implementation using js Map data structure
-
-// Note :- In that map last value is most recently used and first value is least recently used
-
-// GET
-// In get we have to return value from map or -1 if not found
-// Make it most recently used, for that we will have to delete it from map and add it at last
-
-// In set we have to insert value
-// In set firstly we have to check that,
-// if it is already present than we have to delete it from map
-// if capacity is full than we have to delete last element from map and add new one and make it most recently used
-// then we have to add it at last
-
-// Time complexity of get and set is O(1)
-
-
 class LRUCache {
     constructor(capacity) {
         this.capacity = capacity;
-        this.map = new Map();
+        this.cache = capacity;
     }
 
     get(key) {
-        if (this.map.has(key)) {
-            let value = this.map.get(key);
-            this.map.delete(key);
-            this.map.set(key, value);
-            return value;
-        }
-        return -1;
-    }
 
-    put(key, value) {
-        if (this.map.has(key)) {
-            this.map.delete(key);
-        }
-        if (this.map.size >= this.capacity) {
-            this.map.delete(this.map.keys().next().value); // this will return the first key and delete it from the map 
-        }
-
-        this.map.set(key, value);
     }
 }
-
-const lru = new LRUCache(3);
-
-lru.put(1, 1); // Cache is {1=1}
-lru.put(2, 2); // Cache is {1=1, 2=2}
-lru.put(3, 3); // Cache is {1=1, 2=2, 3=3}
-console.log(lru.get(1)); // Returns 1, Cache is {2=2, 3=3, 1=1}
-lru.put(4, 4); // Cache is {3=3, 1=1, 4=4} (removes key 2)
-console.log(lru.get(2)); // Returns -1 (not found)
-console.log(lru.get(3)); // Returns 3, Cache is {1=1, 4=4, 3=3}
-lru.put(5, 5); // Cache is {4=4, 3=3, 5=5} (removes key 1)
-console.log(lru.get(1)); // Returns -1 (not found)
-console.log(lru.get(4)); // Returns 4, Cache is {3=3, 5=5, 4=4}
-console.log(lru.get(5)); // Returns 5, Cache is {3=3, 4=4, 5=5}
-console.log(lru); // {capacity: 3, map: Map {3 => 3, 4 => 4, 5 => 5}}
-
