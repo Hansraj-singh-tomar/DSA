234917f1712f91419930dc57e4823b8d7a518cd9
Hansraj-singh-tomar <hansrajput033@gmail.com>
2024-10-09 21:38:57 +0530

    iterative traversing in DFS

--- Take U Forward/Tree/BinaryTree/CRUD/IterativelyDFSTraversing.js
+++ Take U Forward/Tree/BinaryTree/CRUD/IterativelyDFSTraversing.js
@@ -3,8 +3,8 @@
 class Node {
     constructor(data, left = null, right = null) {
         this.data = data;
-        this.left = null;
-        this.right = null;
+        this.left = left;
+        this.right = right;
     }
 }
 
@@ -49,27 +49,114 @@ class BinaryTree {
         if (root === null) {
             return;
         }
-        console.log(root.data);
-        this.preOrder(root.left);
-        this.preOrder(root.right);
+
+        let stack = [root];
+        let result = [];
+
+        while (stack.length > 0) {
+            const current = stack.pop();
+            result.push(current.data);
+            // console.log(current.data);
+            if (current.right) {
+                stack.push(current.right);
+            }
+            if (current.left) {
+                stack.push(current.left);
+            }
+        }
+
+        return result;
     }
 
     inOrder(root) {
         if (root === null) {
             return;
         }
-        this.inOrder(root.left);
-        console.log(root.data);
-        this.inOrder(root.right);
+
+        let stack = [];
+        let result = [];
+
+        let current = root;
+
+        while (stack.length > 0 || current !== null) {
+            // Traverse left subtree
+            while (current !== null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            // visit the node 
+            current = stack.pop();
+            result.push(current.data);
+
+            // Traverse right subtree
+            current = current.right;
+        }
+
+        return result;
+
     }
 
+    // using two stack
+    // postOrder(root) {
+    //     if (root === null) {
+    //         return [];
+    //     }
+
+    //     let stack1 = [];
+    //     let stack2 = [];
+
+    //     let result = [];
+
+    //     stack1.push(root);
+
+    //     while (stack1.length > 0) {
+    //         let current = stack1.pop();
+
+    //         stack2.push(current?.data);
+
+    //         if (current?.left) {
+    //             stack1.push(current?.left);
+    //         }
+    //         if (current?.right) {
+    //             stack1.push(current?.right);
+    //         }
+    //     }
+
+    //     while (stack2.length > 0) {
+    //         result.push(stack2.pop());
+    //     }
+
+    //     return result;
+    // }
+
+    // using one stack 
     postOrder(root) {
-        if (root === null) {
-            return;
+        let stack = [];
+        let result = [];
+
+        let current = root;
+
+        while (current != null || stack.length > 0) {
+            if (current !== null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                let temp = stack[stack.length - 1].right;
+                if (temp === null) {
+                    temp = stack[stack.length - 1];
+                    stack.pop();
+                    result.push(temp.data);
+                    while (stack.length > 0 && temp === stack[stack.length - 1].right) {
+                        temp = stack.pop();
+                        result.push(temp.data);
+                    }
+                } else {
+                    current = temp;
+                }
+            }
         }
-        this.postOrder(root.left);
-        this.postOrder(root.right);
-        console.log(root.data);
+        return result;
     }
 }
 
@@ -84,11 +171,11 @@ tree.addNode(7);
 tree.addNode(8);
 tree.addNode(9);
 
-tree.preOrder(tree.root); // Output: 1 2 4 8 9 5 3 6 7
+console.log("preOrder traversal", tree.preOrder(tree.root)); // Output: [1 2 4 8 9 5 3 6 7]
 
-tree.inOrder(tree.root); // Output: 8 4 9 2 5 1 6 3 7
+console.log("inOrder traversal", tree.inOrder(tree.root)); // Output: [8 4 9 2 5 1 6 3 7]
 
-tree.postOrder(tree.root); // Output: 8 9 4 5 2 6 7 3 1
+console.log("postOrder traversal", tree.postOrder(tree.root)); // Output: [8 9 4 5 2 6 7 3 1]
 
 console.log(tree);
 // Output:-
--- practice.js
+++ practice.js
@@ -3,8 +3,8 @@
 class Node {
     constructor(data, left = null, right = null) {
         this.data = data;
-        this.left = null;
-        this.right = null;
+        this.left = left;
+        this.right = right;
     }
 }
 
@@ -49,38 +49,114 @@ class BinaryTree {
         if (root === null) {
             return;
         }
-        let queue = [root];
-        let ans = [];
-        while (queue.length > 0) {
-            const current = queue.shift();
-            ans.push(current.data);
 
-            if (current.left) {
-                queue.push(current.left);
-            }
+        let stack = [root];
+        let result = [];
+
+        while (stack.length > 0) {
+            const current = stack.pop();
+            result.push(current.data);
+            // console.log(current.data);
             if (current.right) {
-                queue.push(current.right);
+                stack.push(current.right);
+            }
+            if (current.left) {
+                stack.push(current.left);
             }
         }
-        return ans;
+
+        return result;
     }
 
     inOrder(root) {
         if (root === null) {
             return;
         }
-        this.inOrder(root.left);
-        console.log(root.data);
-        this.inOrder(root.right);
+
+        let stack = [];
+        let result = [];
+
+        let current = root;
+
+        while (stack.length > 0 || current !== null) {
+            // Traverse left subtree
+            while (current !== null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            // visit the node 
+            current = stack.pop();
+            result.push(current.data);
+
+            // Traverse right subtree
+            current = current.right;
+        }
+
+        return result;
+
     }
 
+    // using two stack
+    // postOrder(root) {
+    //     if (root === null) {
+    //         return [];
+    //     }
+
+    //     let stack1 = [];
+    //     let stack2 = [];
+
+    //     let result = [];
+
+    //     stack1.push(root);
+
+    //     while (stack1.length > 0) {
+    //         let current = stack1.pop();
+
+    //         stack2.push(current?.data);
+
+    //         if (current?.left) {
+    //             stack1.push(current?.left);
+    //         }
+    //         if (current?.right) {
+    //             stack1.push(current?.right);
+    //         }
+    //     }
+
+    //     while (stack2.length > 0) {
+    //         result.push(stack2.pop());
+    //     }
+
+    //     return result;
+    // }
+
+    // using one stack 
     postOrder(root) {
-        if (root === null) {
-            return;
+        let stack = [];
+        let result = [];
+
+        let current = root;
+
+        while (current != null || stack.length > 0) {
+            if (current !== null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                let temp = stack[stack.length - 1].right;
+                if (temp === null) {
+                    temp = stack[stack.length - 1];
+                    stack.pop();
+                    result.push(temp.data);
+                    while (stack.length > 0 && temp === stack[stack.length - 1].right) {
+                        temp = stack.pop();
+                        result.push(temp.data);
+                    }
+                } else {
+                    current = temp;
+                }
+            }
         }
-        this.postOrder(root.left);
-        this.postOrder(root.right);
-        console.log(root.data);
+        return result;
     }
 }
 
@@ -95,11 +171,11 @@ tree.addNode(7);
 tree.addNode(8);
 tree.addNode(9);
 
-console.log(tree.preOrder(tree.root)); // Output: 1 2 4 8 9 5 3 6 7
+console.log("preOrder traversal", tree.preOrder(tree.root)); // Output: [1 2 4 8 9 5 3 6 7]
 
-// tree.inOrder(tree.root); // Output: 8 4 9 2 5 1 6 3 7
+console.log("inOrder traversal", tree.inOrder(tree.root)); // Output: [8 4 9 2 5 1 6 3 7]
 
-// tree.postOrder(tree.root); // Output: 8 9 4 5 2 6 7 3 1
+console.log("postOrder traversal", tree.postOrder(tree.root)); // Output: [8 9 4 5 2 6 7 3 1]
 
 console.log(tree);
 // Output:-
