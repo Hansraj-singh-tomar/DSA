23d5245afa292c400487049974e70ce622a8af64
Hansraj-singh-tomar <hansrajput033@gmail.com>
2024-12-04 17:34:16 +0530

    new object question added in that

--- Object Questions/merge_two_arrays_of_objects.js
+++ Object Questions/merge_two_arrays_of_objects.js
@@ -9,14 +9,14 @@ for (let elm of array1) {
     map[elm.id] = elm
 }
 
-// console.log(map);
+// console.log(map); // { 1: { id: 1, name: 'Alice' }, 2: { id: 2, name: 'Bob' } }
 
 
-let mergedArr = array1.map((item) => {
+let mergedArr = array2.map((item) => {
     return { ...item, ...map[item.id] }
 })
 
-console.log(mergedArr);  // [{id: 1, name: 'Alice'}, {id: 2, name: 'Bob'}]
+console.log(mergedArr);  // [{id: 1, name: 'Alice', age: 25}, {id: 2, name: 'Bob', age: 30}]
 
 
 
--- practice.js
+++ practice.js
@@ -1,70 +1,17 @@
-class Node {
-    constructor(data, left = null, right = null) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
+let array1 = [{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }];
+let array2 = [{ id: 1, age: 25 }, { id: 2, age: 30 }];
+
+let map = {}
+
+for (let elm of array1) {
+    map[elm.id] = elm
 }
 
-function InsertIntoBST(root, val) {
-    if (root == null) return new Node(val);
+console.log(map); // { 1: { id: 1, name: 'Alice' }, 2: { id: 2, name: 'Bob' } }
 
-    let curr = root;
+let mergedArr = array2.map((item) => {
+    return { ...item, ...map[item.id] }
+})
 
-    while (true) {
-        if (val < curr.data) {
-            // Move to the left
-            if (curr.left !== null) {
-                curr = curr.left;
-            } else {
-                curr.left = new Node(val);
-                break;
-            }
-        } else {
-            // Move to the right
-            if (curr.right !== null) {
-                curr = curr.right;
-            } else {
-                curr.right = new Node(val);
-                break;
-            }
-        }
-    }
-
-    return root;
-}
-
-
-
-// Example tree construction
-let root = new Node(10);
-
-root.left = new Node(5);
-root.right = new Node(13);
-
-root.left.left = new Node(3);
-root.left.right = new Node(6);
-
-root.right.left = new Node(11);
-root.right.right = new Node(14);
-
-root.left.left.left = new Node(2);
-root.left.left.right = new Node(4);
-
-root.left.right.right = new Node(9);
-
-console.log(InsertIntoBST(root, 7));
-
-// Tree -
-//             10
-//           /    \
-//          5     13
-//         / \    / \
-//        3   6  11 14
-//      / \    \
-//     2   4    9
-//             /
-//            7
-
-
+console.log(mergedArr);
 
